9f03515c1fcb4e1a750825a789044506ec428123
patrick-replogle <patrickr1138@gmail.com>
2019-09-28 09:49:55 -0700

    retooling the styles to use styled-components.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,26 +4,29 @@ import styled from "styled-components";
 const Card = styled.div`
   background-color: rgba(255, 0, 0, 0.3);
   color: black;
-  width: 40%;
-  opacity: 0.5;
+  width: 60%;
+  opacity: 0.8;
   box-shadow: inset 0 0 10px #000000;
+  margin: 20px 0;
 `;
 
 const H2 = styled.h2`
   font-size: 3rem;
   font-weight: bold;
-  margin: 5px 0;
+  margin: 10px 0;
 `;
 
 const P = styled.p`
   font-size: 2rem;
-  margin: 5px 0;
+  margin: 10px 0;
 `;
 
 const CharacterCard = props => {
   return (
     <Card>
       <H2>Name: {props.characters.name}</H2>
+      <P>Birth Year: {props.characters.birth_year}</P>
+      <P>Gender: {props.characters.gender}</P>
       <P>Height: {props.characters.height}</P>
       <P>Mass: {props.characters.mass}</P>
       <P>Hair Color: {props.characters.hair_color}</P>
